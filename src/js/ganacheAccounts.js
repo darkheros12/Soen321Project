7db6c6ec41105477cff94005cd6f5ffc0aa8ed43
Eric-Z0 <ericzhuang603@gmail.com>
2018-03-31 17:48:36 -0400

    accnts array

---
+++ src/js/ganacheAccounts.js
@@ -0,0 +1,52 @@
+//var accountArr;
+GanacheAccounts = {
+
+	accounts: null,
+
+	getAllAccounts: function() {
+		var x;
+		x = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')).eth.
+		getAccounts(function(err, accounts) {
+	 		console.log(accounts);
+	 		if(typeof(accounts) !== 'undefined') {
+	 		GanacheAccounts.accounts = accounts;	
+	 	}
+	 
+	 	return accounts;
+	 //x = accounts;
+		});
+	}
+}
+
+$(function() {
+  $(window).load(function() {
+    GanacheAccounts.getAllAccounts();
+  });
+});
+/*function getAllAccounts()
+{
+
+	// Because of Metamask we have to new web3 to access all the accounts in Ganache
+	/*new Web3(new Web3.providers.HttpProvider('http://localhost:7545')).eth.getAccounts(
+		(err, accounts) => {
+	 console.log(accounts);
+	 return accounts;
+	}).then(function(accnts) 
+	{accountArr = accnts
+	});*/
+
+	/*var x;
+	x = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')).eth.
+	getAccounts(function(err, accounts) {
+	 console.log(accounts);
+	 if(typeof(accounts) !== 'undefined') {
+	 	x = accounts;	
+	 }
+	 
+	 return accounts;
+	 //x = accounts;
+	});
+
+
+	console.log(x);*/
+
